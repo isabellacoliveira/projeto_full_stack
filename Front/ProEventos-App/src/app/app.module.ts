8933c2577211d30b84951d3082a9c1aba0766c07
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2023-01-16 22:05:35 -0300

    adicionando as interfaces e tipando no front-end

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { EventoService } from './services/Evento.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -25,7 +26,7 @@ import { FormsModule } from '@angular/forms';
     CollapseModule.forRoot(),
     FormsModule
   ],
-  providers: [],
+  providers: [EventoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from '@angular/common/http';
+import { Evento } from './../models/Evento';
+import { EventoService } from './../services/Evento.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-  public eventos: any = [];
-  widthImg: number = 150;
-  marginImg: number = 2;
-  showImage: boolean = true;
-  public eventosFiltrados: any = [];
+  public eventos: Evento[] = [];
+  public widthImg: number = 150;
+  public marginImg: number = 2;
+  public showImage: boolean = true;
+  public eventosFiltrados: Evento[] = [];
   public _filtroLista: string = '';
 
   public get filtroLista(): string{
@@ -23,7 +24,7 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  filtrarEventos(filtrarPor: string): any {
+  public filtrarEventos(filtrarPor: string): Evento[] {
     filtrarPor = filtrarPor.toLocaleLowerCase();
     return this.eventos.filter(
       // evento:
@@ -34,20 +35,20 @@ export class EventosComponent implements OnInit {
     )
   }
 
-  showOrHideImage(){
+  public showOrHideImage(): void {
     this.showImage = !this.showImage;
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private eventoService: EventoService) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.getEventos()
   }
 
   public getEventos(): void {
-    this.http.get('https://localhost:5001/api/evento').subscribe(
-      response  => {
-        this.eventos = response,
+    this.eventoService.getEventos().subscribe(
+      (_eventos: Evento[])  => {
+        this.eventos = _eventos,
         this.eventosFiltrados = this.eventos
       } ,
       error => console.log(error)
